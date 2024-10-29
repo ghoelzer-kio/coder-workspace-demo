3a9c797bedc90d09573c93525e6d3d32af8e7b2c
ghoelzer-kio <greg.hoelzer@gmail.com>
2024-10-29 20:34:42 +0000

    demoing sam cli

---
+++ workspace-setup.sh
@@ -0,0 +1,6 @@
+#Downloade AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#Follow Directions in README.md after unzipping
+#Download SAM CLI
+wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+#Unzip and run install per - https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-sam-cli.html
